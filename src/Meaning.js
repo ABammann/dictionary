d05f475d56153e5cade7b2211b35c64a1a84d8ed
ABammann <87564425+ABammann@users.noreply.github.com>
2022-01-24 14:25:49 +0100

    Updated

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Meaning.css";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
@@ -11,7 +10,6 @@ export default function Meaning(props) {
           <div key={index}>
             <p>
               {definiton.definiton}
-              <br />
               <em>{definiton.example}</em>
             </p>
           </div>
