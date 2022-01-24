58211be0ff793a70ac8a734ae3d0595de76a5b79
ABammann <87564425+ABammann@users.noreply.github.com>
2022-01-24 11:43:12 +0100

    New coomit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by Alessandra Bammann. Source at{" "}
+            <a href="https://github.com/ABammann/dictionary">GitHub</a>, hosted
+            at Netifly.
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
