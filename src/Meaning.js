7e550a4bdb23ad26f34c155327dfcf91e7337733
ABammann <87564425+ABammann@users.noreply.github.com>
2022-01-24 14:47:38 +0100

    Finally corrected

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./Meaning.css";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>{props.meanings.partOfSpeech}</h3>
+      <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
               {definition.definition}
+              <br />
               <em>{definition.example}</em>
             </p>
           </div>
