2d5fbe81b7ad5761b883e31b9936a0ab4d2c5951
ABammann <87564425+ABammann@users.noreply.github.com>
2022-01-31 10:33:46 +0100

    Styling for all

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -24,9 +24,16 @@ export default function Dictionary() {
 
   return (
     <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="Search" onChange={handleKeywordChange} />
-      </form>
+      <section>
+        <h1>What word do you want to look up?</h1>
+        <form onSubmit={search}>
+          <input
+            type="Search"
+            onChange={handleKeywordChange}
+            defaultValue="Type here"
+          />
+        </form>
+      </section>
       <Results results={results} />
     </div>
   );
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,14 +1,14 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         {" "}
-        Listen🔊
+        Listen
       </a>
-      <br />
-      {props.phonetic.text}
+      🔊 {"  "}/{props.phonetic.text}/
     </div>
   );
 }
